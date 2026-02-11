3b0dc4ac72226128b8ca861cd8fadef78864509e
Monicat263 <mda263@bol.com.br>
2026-02-11 16:03:42 -0300

    commit inicial

---
+++ src/main/java/com/mvalentin/noticacao/NoticacaoApplication.java
@@ -0,0 +1,13 @@
+package com.mvalentin.noticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvalentin/noticacao/NoticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvalentin.noticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
