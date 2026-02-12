319973960a7128161df9d0558529d5fa177d87fd
Monicat263 <mda263@bol.com.br>
2026-02-12 17:27:08 -0300

    Implementação envio de email

---
+++ src/main/java/com/mvalentin/noticacao/business/dto/TarefasDto.java
@@ -0,0 +1,28 @@
+package com.mvalentin.noticacao.business.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mvalentin.noticacao.infrastructure.enums.StatusNotificacaoEnum;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TarefasDto {
+
+    private String id;
+    private String nomeTarefa;
+    private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+    private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+}
